2d78580309ee86037cc8dd1b5a745654c6e12cd4
Davide Francesco Riccio <ledrazors@gmail.com>
2023-06-08 03:30:30 +0200

    Primo commit

---
+++ src/co/develhope/introduction/Start.java
@@ -0,0 +1,7 @@
+package co.develhope.introduction;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello develhope java student " + "15/06/1998");
+    }
+}
